978b0dca8a49922e2cc0229a8fef81f4a37ca445
Natalia Rotaras <nrotaras@ausy-group.com>
2019-11-29 16:21:17 +0200

    Develop REST APIs
    Develop REST APIs

--- europe-countries/src/main/java/com/europe/europecountries/controllers/CountriesController.java
+++ europe-countries/src/main/java/com/europe/europecountries/controllers/CountriesController.java
@@ -1,14 +1,14 @@
 package com.europe.europecountries.controllers;
 
-import com.europe.europecountries.models.CountryLanguage;
-import com.europe.europecountries.repositories.CityRepository;
+import com.europe.europecountries.exceptions.CountryNotFoundException;
 import com.europe.europecountries.models.Country;
 import com.europe.europecountries.repositories.CountryRepository;
-import com.europe.europecountries.repositories.LanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/europe")
@@ -17,67 +17,46 @@ public class CountriesController {
     @Autowired
     private final CountryRepository countryRepository;
 
-    @Autowired
-    private final CityRepository cityRepository;
-
-    @Autowired
-    private final LanguageRepository langRepository;
-
-    public CountriesController(CountryRepository countryRepository, CityRepository cityRepository, LanguageRepository langRepository) {
+    public CountriesController(CountryRepository countryRepository) {
         this.countryRepository = countryRepository;
-        this.cityRepository = cityRepository;
-        this.langRepository = langRepository;
     }
 
+    //GET all Countries from a list
     @GetMapping("/countries")
     public List<Country> findAll() {
         return countryRepository.findAll();
     }
 
-    @PutMapping("/countries/")
-    public Country addCountry(@RequestBody Country newCountry){
-        newCountry.getLanguages().get(0).getCountry();
+    @GetMapping("/countries/{countryId}")
+    Country oneCountry(@PathVariable Integer countryId) {
+        return countryRepository.findById(countryId).orElseThrow(() -> new CountryNotFoundException());
+    }
+
+    //POST a new Country object
+    @PostMapping("/countries")
+    public Country addCountry(@Valid @RequestBody Country newCountry) {
         return countryRepository.save(newCountry);
     }
 
+    // PUT another information about one country
+    @PutMapping("/countries/{countryId}")
+    public Country updateCountry(@PathVariable Integer countryId, Country upCountry) {
+        return countryRepository.findById(countryId).map(country -> {
+            country.setCode(upCountry.getCode());
+            country.setName(upCountry.getName());
+            country.setRegion(upCountry.getRegion());
+            country.setPopulation(upCountry.getPopulation());
+            country.setSurfaceArea(upCountry.getSurfaceArea());
+            return countryRepository.save(country);
+        }).orElseThrow(() -> new CountryNotFoundException());
+    }
 
-   /* @PutMapping("/countries/{id}")
-    public Country updateCountry(@RequestBody Country newCountry, @PathVariable Long id) {
+    @DeleteMapping("/countries/{countryId}")
+    public void deleteCountry(@PathVariable Integer countryId) {
+        if (countryRepository.existsById(countryId)) {
+            countryRepository.deleteById(countryId);
+        }
 
-
-        Country updatedCountry = countryRepository.findById(id)
-                .map(country -> {
-                    country.setCode(newCountry.getCode());
-                    country.setName(newCountry.getName());
-                    country.setRegion(newCountry.getRegion());
-                    country.setPopulation(newCountry.getPopulation());
-                    country.setSurfaceArea(newCountry.getSurfaceArea());
-                    // country.setLanguages(langs);
-                    return countryRepository.save(country);
-                }).orElseGet(
-                        () -> {
-//                            newCountry.setId(id);
-//                            langRepository.save(newCountry.getLanguages().get(0));
-                            CountryLanguage c = new CountryLanguage();
-                            c.setLanguage("asdasd");
-
-                            langRepository.save(c);
-                            newCountry.getLanguages().add(c);
-                            return countryRepository.save(newCountry);
-                        });
-
-
-//        List<CountryLanguage> langs = newCountry.getLanguages();
-//        for (CountryLanguage lang : langs){
-//            //lang.setCountry(updatedCountry);
-//            langRepository.save(lang);
-//        }
-        return updatedCountry;
-    }*/
-
-    @GetMapping("/languages")
-    public List<CountryLanguage> allLanguages() {
-        return langRepository.findAll();
     }
 
 }
---
+++ europe-countries/src/main/java/com/europe/europecountries/controllers/LanguagesController.java
@@ -0,0 +1,41 @@
+package com.europe.europecountries.controllers;
+
+import com.europe.europecountries.exceptions.NotLanguageFindException;
+import com.europe.europecountries.models.CountryLanguage;
+import com.europe.europecountries.repositories.CountryRepository;
+import com.europe.europecountries.repositories.LanguageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/europe/countries")
+public class LanguagesController {
+    @Autowired
+    private final LanguageRepository languageRepository;
+
+    @Autowired
+    private final CountryRepository countryRepository;
+
+    public LanguagesController(LanguageRepository languageRepository, CountryRepository countryRepository) {
+        this.languageRepository = languageRepository;
+        this.countryRepository = countryRepository;
+    }
+
+    @GetMapping("/{countryId}/languages")
+    public Page<CountryLanguage> findLanguageByCountryId(@PathVariable(value = "countryId") Integer countryId, Pageable pageable) {
+        return languageRepository.findLanguageByCountryId(countryId, pageable);
+    }
+
+    @PostMapping("/{countryId}/languages")
+    public CountryLanguage createLanguage(@PathVariable(value = "countryId") Integer countryId, @Valid @RequestBody CountryLanguage language) {
+        return countryRepository.findById(countryId).map(country -> {
+            language.setCountry(country);
+            return languageRepository.save(language);
+        }).orElseThrow(() -> new NotLanguageFindException());
+    }
+
+}
